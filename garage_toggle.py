c3265d1e4c7dbe0f5e3c66be9641541fd68b964f
Brian Vicente Sr <sailbv@gmail.com>
2019-01-05 15:15:54 -0800

    Initial project description
    Has basic info on what the project is about

---
+++ garage_toggle.py
@@ -0,0 +1,9 @@
+''' 
+garage_toggle.py
+
+        Garage Toggle manages automatic closing of a garage given certain conditions
+        The main problem it's to solve is when a garage is left open.
+        A first goal is to have the system use TIME to determine if the garage should be closed and 
+        sends signals to close it
+'''
+
